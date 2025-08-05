ebe75609cd4cf5bcc30bf5e6e29e8b6708067f57
Aniruddha Kide <aniruddhakide16@gmail.com>
2025-08-05 21:20:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For development only, restrict in production
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
